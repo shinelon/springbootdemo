512dae58bd8f30c3f315b542b184781c560578f4
shinelon <syq_sunshine@163.com>
2016-10-25 17:47:49 +0800

    mybatis

--- src/test/java/com/spring/SpringbootdemoApplicationTests.java
+++ src/test/java/com/spring/SpringbootdemoApplicationTests.java
@@ -2,13 +2,17 @@ package com.spring;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.spring.service.sys.UserService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootdemoApplicationTests {
-
+	@Autowired
+	private UserService userService;
 	@Test
 	public void contextLoads() {
 	}
